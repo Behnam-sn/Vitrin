2c4dec4dd7724107fd574b212dcb15dbbc6edd62
Behnam <salehinezhad.b@gmail.com>
2018-11-26 09:08:46 +0330

    icon

--- js/product.js
+++ js/product.js
@@ -1,11 +1,17 @@
 var info = document.querySelector("#info");
 var comment = document.querySelector("#comment");
 
+var infoIcon = document.querySelector("#info-icon");
+var commentIcon = document.querySelector("#comment-icon");
+
 function showI() {
     info.classList.add("appear");
     info.classList.remove("disappear");
     comment.classList.add("disappear");
     comment.classList.remove("appear");
+
+    infoIcon.classList.remove("icon-deactive");
+    commentIcon.classList.add("icon-deactive");
 }
 
 function showC() {
@@ -13,4 +19,8 @@ function showC() {
     info.classList.remove("appear");
     comment.classList.add("appear");
     comment.classList.remove("disappear");
+
+    infoIcon.classList.add("icon-deactive");
+    commentIcon.classList.remove("icon-deactive");
+
 }
