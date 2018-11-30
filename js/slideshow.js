a60c951df68fa9031735fb3450a81e78685483f9
Behnam <salehinezhad.b@gmail.com>
2018-11-30 04:50:00 +0330

    rap god

---
+++ js/slideshow.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.car').slick({
+        autoplay: false,
+        slidesToShow: 4,
+        infinite: false,
+        vertical: true,
+        useTransform: true,
+        cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
+        prevArrow: $('.up'),
+        nextArrow: $('.down'),
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    vertical: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    vertical: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    vertical: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
